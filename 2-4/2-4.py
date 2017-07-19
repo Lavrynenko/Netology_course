f50133c7526d264931a774511f95a8019a45f30a
Oleksiy Lavrynenko <oleksiy.lavrynenko@outlook.com>
2017-07-19 23:38:28 +0300

    Комментарий

--- 2-4/2-4.py
+++ 2-4/2-4.py
@@ -9,20 +9,6 @@ def get_a_list():
 
 def main():
     all_names_files = get_a_list()
-    # while True:
-    #     all_results = list()
-    #     questions = input("Какой файл ищем?\n >>> ")
-    #     questions = questions.lower()
-    #     for file in all_names_files:
-    #         with open(file, 'r') as f:
-    #             line = f.read()
-    #             if questions in line:
-    #                 all_results.append(file)
-    #     all_names_files = all_results
-    #     pprint(all_results)
-    #     pprint("Всего найдено: {} файлов в all_results".format(len(all_results)))
-
-#Как просили - вроде должно работать
     while True:
         all_results = list()
         questions = input("Введите поисковый запрос: ").lower()
