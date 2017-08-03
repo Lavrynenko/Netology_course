bd378fe0b9ce805e63aef4ae41901fa7048e56ae
Oleksiy Lavrynenko <oleksiy.lavrynenko@outlook.com>
2017-08-04 00:34:17 +0200

    Комментарий

---
+++ Parser_link/parser.py
@@ -0,0 +1,20 @@
+#import requests
+import requests
+import re
+import urllib
+
+#Открываем файл со ссылкой на нужную страницу
+f = open('link.txt','r', encoding='utf8')
+link = f.readline()
+print(link)
+
+#Получаем ответ страницы и выводим исходный код страницы
+response = requests.get(link)
+print (response.content)
+
+#Сохраняем исходный код страницы во временный файл
+f = open('temp.txt', 'wb')
+text_for_file = response.content
+f.write(text_for_file)
+f.close()
+print('Исходный код страницы сохранен в файл')
