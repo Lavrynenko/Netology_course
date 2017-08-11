6a406f1c414721ccb3b692f9601a274f1c10d0b4
Oleksiy Lavrynenko <oleksiy.lavrynenko@outlook.com>
2017-08-12 02:29:06 +0300

    Комментарий

--- Parser_link/parser.py
+++ Parser_link/parser.py
@@ -28,20 +28,85 @@ print('Исходный код страницы сохранен в файл')
 pars = []
 with open('temp.txt', 'r', encoding='utf8') as f:
     pars = f.read()
-print (pars)
+#print (pars)
 regxp = '(?<=photoUrl&quot;:&quot;)(https:\/\/cdn\.inmyroom\.ru\/uploads\/photo.*?\.jpg)(?=&quot;}\"><noscript)'
 result = re.findall(regxp, pars)
-print (result)
+#print (result)
 
-result = str(result)
-f = open('fulllink.txt', 'w')
-f.write(result)
-f.close()
+result = list(result)
+#print ('Вывод строк')
+#print (result[2])
+counter = 1
+for link in result:
+    print(link)
+    url = link
+    img = urllib.request.urlopen(url).read()
+    out = open("counter.jpg", "wb")
+    out.write(img)
+    out.close
+    counter = counter + 1
+
+    out = open("img.jpg", "wb")
+
+
+    # for x in result:
+    #   print (x)
+    # url = x
+    # img = urllib.request.urlopen(url).read()
+    # out = open(x,',','jpg', 'wb')
+    # out.write(img)
+    # out.close
+
+
+# # print (result.split(,))
+# #print (result(str.partition(', ')))
+#
+# f = open('fulllink.txt', 'w')
+# f.write(result)
+# f.close()
+# print('Ссылки сохранены в файл')
+#
+# with open('fulllink.txt','r') as f:
+#     for link in f:
+#         link = list()
+#         print(f)
+
+
+    # def get_dishes():
+    #     dishes_all = dict()
+    #     with open('menu.txt', 'r') as f:
+    #         for line in f:
+    #             dishes_value = list()
+    #             dishes = line.strip()
+    #             ingridient_count = int(f.readline().strip())
+    #             for _ in range(ingridient_count):  # _ одноразовая переменная
+    #                 ingredient = f.readline().strip().split(" | ")
+    #                 i = {"ingridient_name": ingredient[0], "quantity": int(ingredient[1]), "measure": ingredient[2]}
+    #                 dishes_value.append(i)
+    #             f.readline()
+    #             dishes_all.update({dishes: dishes_value})
+
+
+
+
+
+
+#Тестим
+# result = list(result)
+# for x in result:
+#   print (x)
+  # url = x
+  # img = urllib.request.urlopen(url).read()
+  # out = open(x,',','jpg', 'wb')
+  # out.write(img)
+  # out.close
+
 
 #stream = urlopen(url)
 
-url = 'https://cdn.inmyroom.ru/uploads/photo/file/cf/cf01/original_cf016769-0086-4a61-8303-143ebe95879f.jpg'
-img = urllib.request.urlopen(url).read()
-out = open("img.jpg", "wb")
-out.write(img)
-out.close
+#Кусок для сохранения фотографий по ссылкам
+# url = 'https://cdn.inmyroom.ru/uploads/photo/file/cf/cf01/original_cf016769-0086-4a61-8303-143ebe95879f.jpg'
+# img = urllib.request.urlopen(url).read()
+# out = open("img.jpg", "wb")
+# out.write(img)
+# out.close
