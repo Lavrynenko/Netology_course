08f29015b3d654125118f126c4e363bc5a95b3b3
Oleksiy Lavrynenko <oleksiy.lavrynenko@outlook.com>
2017-10-22 19:14:05 +0300

    Комментарий

--- Mario/while_endless_cycle.py
+++ Mario/while_endless_cycle.py
@@ -3,10 +3,10 @@
 '''
 
 while True:
-    maximum = int(input('Введите число-максимум: '))
-    quantity = int(input('Введите число, которое не больше введенного числа-максимума: '))
-    if maximum < quantity:
-        print('Введенное число должно быть МЕНЬШЕ или равно ',maximum ,' :) Повторите!: ')
-    else:
-        print('Ну наконец-то! :) ')
-        break
+  maximum = int(10)
+  quantity = int(input('Введите число: '))
+  if maximum < quantity:
+    print ('Неправильно, повторите! ')
+  else:
+    print('Ok! ')
+    break
