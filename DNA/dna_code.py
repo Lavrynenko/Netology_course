e8eadfaa0b86a426dfb16ae995bfe2bfe00c9d45
Lavrynenko <oleksiy@lavrynenko.com>
2018-05-28 22:55:50 +0300

    Комментарий

---
+++ DNA/dna_code.py
@@ -0,0 +1,18 @@
+dna_code = str(input('Введите последовательность кода ДНК: \n'))
+print('Введен ДНК-код: ', dna_code)
+dna_code = dna_code.lower()
+dna_a = int(0)
+dna_g = int(0)
+dna_c = int(0)
+dna_t = int(0)
+for symbol in dna_code:
+    #print(symbol)
+    if symbol == 'a':
+        dna_a = dna_a + 1
+    elif symbol == 'g':
+        dna_g = dna_g + 1
+    elif symbol == 'c':
+        dna_c = dna_c + 1
+    elif symbol == 't':
+        dna_t = dna_t + 1
+print('Во введенном коде: \n Аденина: ', dna_a, '\n Гуанина: ', dna_g, '\n Цитозина: ', dna_c, '\n Тимина: ', dna_t)
