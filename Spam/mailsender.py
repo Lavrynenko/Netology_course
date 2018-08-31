170bc17355b6dbe62ea0abf046a901bc3afacdfd
Lavrynenko <oleksiy@lavrynenko.com>
2018-09-01 00:03:56 +0300

    Комментарий

--- Spam/mailsender.py
+++ Spam/mailsender.py
@@ -5,13 +5,13 @@ from email.header import Header
 def send_mail():
 	mailsender = smtplib.SMTP('smtp.gmail.com', 587)
 	mailsender.starttls()
-	mailsender.login('oleksiy.lavrynenko@gmail.com', 'v3B7In19Ry2')
-	mail_recipient = 'oleksiy.lavrynenko@outlook.com'
+	mailsender.login('Адрес почты отправителя', 'Пароль к почте отправителя')
+	mail_recipient = 'Адрес почты получателя'
 	mail_subject = 'Тема сообщения'
 	mail_body = 'Текст сообщения'
 	msg = MIMEText(mail_body, 'plain', 'utf-8')
 	msg['Subject'] = Header(mail_subject, 'utf-8')
-	mailsender.sendmail('oleksiy.lavrynenko@gmail.com', mail_recipient, msg.as_string())
+	mailsender.sendmail('Адрес почты отправителя', mail_recipient, msg.as_string())
 	mailsender.quit()
 	print('Сообщение на адрес', mail_recipient, 'отправлено')
 send_mail()
